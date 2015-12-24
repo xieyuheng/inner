92eabf13c397d192620aa93d596a4ed80bc1aa8f
XIE Yuheng <xyheme@gmail.com>
2015-12-25 01:53:07 +0800

    回收內存的時機

--- lang/prolog/cicada.pl
+++ lang/prolog/cicada.pl
@@ -8,3 +8,24 @@ add(succ(M), N, succ(O)):-
 append([], List2, List2).
 append([Head1|Tail1], List2, [Head1|Tail2]):-
   append(Tail1, List2, Tail2).
+
+prefix(P, L):-
+  append(P, _, L).
+
+suffix(S, L):-
+  append(_, S, L).
+
+sublist(Sub, L):-
+  suffix(S, L),
+  prefix(Sub, S).
+
+%% reverse([], []).
+%% reverse([Head|Tail], R):-
+%%   append(R1, [Head], R),
+%%   reverse(Tail, R1).
+
+reverse(L, R):-
+  reverse(L, R, []).
+reverse([], R, R).
+reverse([Head|Tail], R, Ac):-
+  reverse(Tail, R, [Head|Ac]).
